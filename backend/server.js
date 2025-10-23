b91d82955beab7e9be6c7a7c0e2df5dec7553645
DevPastey <emmanuelolabodee@outlook.com>
2025-10-23 10:11:28 +0100

    Implement cart routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -17,6 +18,8 @@ app.use(cookieParser()); //allows you access the cookies (req.cookies)
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+
 
 
 connectDB().then(
