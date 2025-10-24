a08491229adea14595322e0363265375785029b9
DevPastey <emmanuelolabodee@outlook.com>
2025-10-24 20:56:15 +0100

    Implement coupon routes

--- backend/controllers/coupon.controller.js
+++ backend/controllers/coupon.controller.js
@@ -19,8 +19,21 @@ export const validateCoupon = async (req, res) => {
             res.status(404).json({ message: "Coupon not found" });
         }
 
-        if(coupon.expirationDate < new Date())
+        if(coupon.expirationDate < new Date()) {
+            coupon.isActive = false;
+            await coupon.save();
+            return res.status(404).json({ message: "Coupon expired"})
+        };
+
+        res.json(
+            {   message: "Coupon is valid",
+                code: coupon.code,
+                discountPercentage: coupon.discountPercentage,
+            }
+  
+        );
     } catch (error) {
-        
+        console.log("Error in validateCoupon controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
     }
 };
