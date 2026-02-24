b0d253b428faf89787b8848a15ac976be0bdc083
Emmanuel Olabode <emmanuelolabodee@outlook.com>
2026-02-24 04:15:53 -0800

    Log connection status for Redis host

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -11,4 +11,6 @@ export const redis = new Redis({
 
 // redis.set("foo", "bar");
 const res = await redis.get("foo");
+console.log("Connecting to host:", process.env.REDIS_URL ? "Variable found" : "VARIABLE MISSING!");
+
 console.log(res)
