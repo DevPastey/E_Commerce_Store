891840d1559ead0b5ff8c8461170f176107cd3eb
DevPastey <emmanuelolabodee@outlook.com>
2026-05-19 13:44:09 +0100

    Refactor code structure for improved readability and maintainability

--- frontend/src/constants/images.ts
+++ frontend/src/constants/images.ts
@@ -1,10 +1,20 @@
 import jeans from "../assets/jeans.jpg";
 import bags from "../assets/bags.jpg";
 import glasses from "../assets/glasses.png";
-import jackets from "../assets/jackets.jpg";
 import shoes from "../assets/shoes.jpg";
-import suits from "../assets/suits.jpg";
-import tshirts from "../assets//tshirts.jpg";
+import sports from "../assets/sports.webp";
+import kitchen from "../assets/kitchen.jpg";
+import appliances from "../assets/appliances.jpg";
+import accessories from "../assets/accessories.jpg";
+import home from "../assets/home.jpg";
+import jewelry from "../assets/jewelry.jpg";
+import cleaning from "../assets/cleaning.jpg";
+import bathroom from "../assets/bathroom.jpg";
+
+
+
+
+
 
 
 
@@ -12,15 +22,14 @@ export const categories = [
     {href: "/apparel", name: "Apparel", imageURL: jeans},
     {href: "/bags", name: "Bags", imageURL: bags},
     {href: "/glasses", name: "Glasses", imageURL: glasses},
-    {href: "/jackets", name: "Jackets", imageURL: jackets},
     {href: "/shoes", name: "Shoes", imageURL: shoes},
-    {href: "/sports", name: "Sports", imageURL: suits},
-    {href: "/accessories", name: "Accessories", imageURL: tshirts},
-    {href: "/appliances", name: "Appliances", imageURL: tshirts},
-    {href: "/bathroom", name: "Bathroom", imageURL: tshirts},
-    {href: "/cleaning", name: "Cleaning", imageURL: tshirts},
-    {href: "/home", name: "Home", imageURL: tshirts},
-    {href: "/kitchen", name: "Kitchen", imageURL: tshirts},
-    {href: "/jewelry", name: "Jewelry", imageURL: tshirts},
+    {href: "/sports", name: "Sports", imageURL: sports},
+    {href: "/accessories", name: "Accessories", imageURL: accessories},
+    {href: "/appliances", name: "Appliances", imageURL: appliances},
+    {href: "/bathroom", name: "Bathroom", imageURL: bathroom},
+    {href: "/cleaning", name: "Cleaning", imageURL: cleaning},
+    {href: "/home", name: "Home", imageURL: home},
+    {href: "/kitchen", name: "Kitchen", imageURL: kitchen},
+    {href: "/jewelry", name: "Jewelry", imageURL: jewelry},
 
 ];
